a256f6c22f93ca5d18bd9c79211e7be82ecb34d5
shreysahgal <shreysahgal@gmail.com>
2018-04-08 14:33:01 -0400

    added stuff

--- Block.py
+++ Block.py
@@ -1,10 +1,12 @@
 
 class Block:
+
     def __init__(self, x, y, w, h):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
+
     def show(self):
         rectMode(CENTER)
-        rect(x, y, w, h)
+        rect(self.x, self.y, self.w, self.h)
--- Player.py
+++ Player.py
@@ -4,31 +4,35 @@ class player(object):
     xspeed = 0
     yspeed = 0
     radius = 0
+
     def __init__(self, y, yspeed, xspeed, radius):
         self.y = y
         self.yspeed = yspeed
         self.xspeed = xspeed
         self.radius = radius
-    
+
     def display():
-        ellipse(x,y,radius,radius)
-    
+        ellipse(x, y, radius, radius)
+
     def moveUp():
         self.y -= self.yspeed
-    
+
     def moveDown():
         self.y += self.yspeed
-    
+
     def moveRight():
         self.x += self.xspeed
-    
+
+
 def setup():
-    size(600,600)
-    playerObj = new player(displayHeight/2,5,3,10)
-    
+    size(600, 600)
+    playerObj = player(displayHeight / 2, 5, 3, 10)
+
+
 def draw():
     playerObj.moveRight()
     playerObj.display()
+
 
 def keyPressed():
     if key == CODED:
@@ -36,5 +40,3 @@ def keyPressed():
             playerObj.moveUp()
         elif keyCode == DOWN:
             playerObj.moveDown()
-        
-    
